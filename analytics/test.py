fe48f1a7627abf65427b6907ca55ec113f1cdc84
Marc Dumon <marcdumon@msn.com>
2020-07-24 20:03:53 +0200

    Initial commit analytics

--- analytics/test.py
+++ analytics/test.py
@@ -4,9 +4,10 @@
 # md
 # --------------------------------------------------------------------------------------------------------
 
+import pandas as pd
 from pymongo import MongoClient
 
-database = 'twitter_database_xxx'
+database = 'test_full'
 
 
 def get_collection(collection_name):  # Todo: same function in many modules. Put in tools?
@@ -20,13 +21,22 @@ tweets = get_collection('tweets')
 profiles = get_collection('profiles')
 
 
-f={}
-
-u={
-
-}
-
+def get_tweets_user_day():
+    pl = [{
+        '$group': {
+            '_id': {'username': '$username', 'date': '$date'},
+            'count': {'$sum': 1}}},
+        {'$sort': {'_id.username': 1, '_id.date': 1}},
+        {'$project': {
+            '_id': 0,
+            'username': '$_id.username',
+            'date': '$_id.date',
+            'count': 1}}
+    ]
+    result = tweets.aggregate(pl, allowDiskUse=True)
+    result = list(result)
+    return pd.DataFrame(result, columns=['username', 'date', 'count'])
 
 
 if __name__ == '__main__':
-    pass
+    get_tweets_user_day()
